17397e8d427746c65a805bf269fbc0dfb1b26282
Trajan McGill <code@trajanmcgill.com>
2013-03-21 12:31:28 -0500

    Set up grunt and some tasks for it. Issue: [not imported from prior issue tracker], [not imported from prior issue tracker]

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt)
+{
+	// Project configuration.
+	grunt.initConfig(
+		{
+			pkg: grunt.file.readJSON("package.json"),
+			
+			jshint:
+			{
+				ConcertJS:
+				{
+					options: {},
+
+					src: ["ConcertJS/Source/Concert.js"]
+				},
+
+				ConcertJSmin:
+				{
+					options: {},
+
+					src: ["Build/Concert.min.js"]
+				}
+			},
+
+			uglify:
+			{
+				BaseObject:
+				{
+					src: ["ConcertJS/Components/BaseObject/BaseObject.js"],
+					dest: "Build/BaseObject.min.js"
+				},
+			
+				ConcertJS:
+				{
+					options: { banner: "/*! <%= pkg.name %> <%= pkg.version %> */\n" },
+					
+					src: ["ConcertJS/Source/Concert.js"],
+					dest: "Build/Concert.min.js"
+				},
+
+				MinMax:
+				{
+					options: { beautify: true },
+					src: ["Build/Concert.min.js"],
+					dest: "Build/Concert.min.max.js"
+				}
+			}
+		});
+	
+	// Load the plugins
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	
+	// Default task
+	grunt.registerTask("default", ["uglify:ConcertJS"]);
+
+	// Other tasks
+	grunt.registerTask("minmax", ["uglify:ConcertJS", "uglify:MinMax"]);
+};
