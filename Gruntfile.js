2a619af7127c2d9db61efbee2148bfaf4ff34a20
Trajan McGill <code@trajanmcgill.com>
2013-03-21 13:14:40 -0500

    A couple of adaptations to BaseObject with the expectation that it will be inserted inside Concert.js instead of existing in a stand-alone fashion, also, got it to pass jshint. Issue: [not imported from prior issue tracker], [not imported from prior issue tracker]

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt)
 
 				BaseObject:
 				{
-					options: { strict: false },
+					options: { strict: false, unused: false },
 					src: ["ConcertJS/Components/BaseObject/BaseObject.js"]
 				},
 
