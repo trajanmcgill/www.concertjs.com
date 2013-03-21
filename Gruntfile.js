1cf9a28e09c24abea4643b46a27409dac6dcc008
Trajan McGill <code@trajanmcgill.com>
2013-03-21 12:41:38 -0500

    Fix to jshint BaseObject target definition. Issue: [not imported from prior issue tracker]

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt)
 				BaseObject:
 				{
 					options: {},
-					src: ["Components/BaseObject/BaseObject.js"]
+					src: ["ConcertJS/Components/BaseObject/BaseObject.js"]
 				},
 
 				ConcertJS:
