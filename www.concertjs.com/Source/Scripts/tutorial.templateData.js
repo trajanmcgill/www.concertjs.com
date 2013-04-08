2c30c2dcf7f0656742b7f9b0de55ee804398d634
Trajan McGill <code@trajanmcgill.com>
2013-04-07 21:44:34 -0500

    Set up some basic template files that will be used in building the tutorial pages. Issue: #3, #8

---
+++ www.concertjs.com/Source/Scripts/tutorial.templateData.js
@@ -0,0 +1,10 @@
+﻿/*<template:targetDef target="tutorial.js" template="tutorial.template.js"></template:targetDef>*/
+
+/*<template:data target="tutorial.js" section="Script">*/
+(function()
+{
+	"use strict";
+
+	
+})();
+/*</template:data>*/
