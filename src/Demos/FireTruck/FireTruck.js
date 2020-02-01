bcd35a457d5d32342a76d0a9a3af735f7e643465
Trajan McGill <code@trajanmcgill.com>
2020-02-01 12:24:52 -0600

    Updated sprites (fire truck) demo.
    Closes: #42
    Issue: #65, #6

--- src/Demos/FireTruck/FireTruck.js
+++ src/Demos/FireTruck/FireTruck.js
@@ -1,12 +1,10 @@
-﻿/// <reference path="~/Components/Concert.js/Concert.js" />
-/* exported demoSequence */
-
+﻿/* exported demoSequence */
 var demoSequence =
 (function ()
 {
 	"use strict";
 
-	var wheelSpriteWidth = 45, wheelFramesPerRotation = 60,
+	const wheelSpriteWidth = 45, wheelFramesPerRotation = 60,
 		wheelCircumference = Math.PI * wheelSpriteWidth,
 		totalWheelRotations = 57,
 		totalDistance = wheelCircumference * totalWheelRotations,
@@ -15,15 +13,43 @@ var demoSequence =
 		roadRepeatLength = 1007, skylineRepeatLength = 1209,
 		totalSequenceTime = 20000,
 		lightsSpriteWidth = 28, flashesPerSecond = 1,
-		finalLightsFrame = totalSequenceTime / 1000 * flashesPerSecond * 2;
+		finalLightsFrame = totalSequenceTime / 1000 * flashesPerSecond * 2,
+		animationLoopCount = 1;
 
-	var mainSequence = new Concert.Sequence();
+	let mainSequence = new Concert.Sequence();
 
+	// Most of the animations added below will use these settings.
 	mainSequence.setDefaults({ unit: "px", applicator: Concert.Applicators.Style, easing: Concert.EasingFunctions.QuadInOut });
 
 	mainSequence.addTransformations(
 		[
-			// Spinning tires (tire rotation frames defined on sprite sheet)
+			// Moving road (closest layer of parallax): Simply move the background image (a repeatable tile) over time.
+			// The modulo modifier will loop the image back to the start position every time it has gone the roadRepeatLength distance.
+			{
+				target: document.getElementById("Road"),
+				feature: "background-position",
+				unit: "px",
+				calculatorModifiers: { modulo: roadRepeatLength },
+				keyframes: { times: [0, totalSequenceTime], values: [0, -1 * totalDistance * roadSpeed] }
+			},
+
+			// Moving skyline (second layer of parallax): Works the same as the road above, but the speed is different.
+			{
+				target: document.getElementById("City"),
+				feature: "background-position",
+				calculatorModifiers: { modulo: skylineRepeatLength },
+				keyframes: { times: [0, totalSequenceTime], values: [0, -1 * totalDistance * skylineSpeed] }
+			},
+
+			// Moving clouds (furthest layer of parallax): Works just as road and skyline above, but at a third independent rate of motion.
+			{
+				target: document.getElementById("Clouds"),
+				feature: "background-position",
+				keyframes: { times: [0, totalSequenceTime], values: [0, -1 * totalDistance * skySpeed] }
+			},
+
+			// Spinning tires: Tire rotation frames are defined on a sprite sheet.
+			// They are animated by placing the right sprite for the present tire position into the visible area.
 			{
 				targets: [document.getElementById("RearTireDiv"), document.getElementById("FrontTireDiv")],
 				feature: ["background-position", "background-position"],
@@ -38,34 +64,10 @@ var demoSequence =
 				calculatorModifiers: { multiply: -1 * lightsSpriteWidth, round: lightsSpriteWidth, modulo: lightsSpriteWidth * 2 },
 				easing: Concert.EasingFunctions.ConstantRate,
 				keyframes: { times: [0, totalSequenceTime], values: [0, finalLightsFrame] }
-			},
-
-			// Moving road (closest layer of parallax)
-			{
-				target: document.getElementById("Road"),
-				feature: "background-position",
-				unit: "px",
-				calculatorModifiers: { modulo: roadRepeatLength },
-				keyframes: { times: [0, totalSequenceTime], values: [0, -1 * totalDistance * roadSpeed] }
-			},
-
-			// Moving skyline (second layer of parallax)
-			{
-				target: document.getElementById("City"),
-				feature: "background-position",
-				calculatorModifiers: { modulo: skylineRepeatLength },
-				keyframes: { times: [0, totalSequenceTime], values: [0, -1 * totalDistance * skylineSpeed] }
-			},
-
-			// Moving clouds (furthest layer of parallax)
-			{
-				target: document.getElementById("Clouds"),
-				feature: "background-position",
-				keyframes: { times: [0, totalSequenceTime], values: [0, -1 * totalDistance * skySpeed] }
 			}
 		]);
 
-	document.getElementById("GoButton").onclick = function () { mainSequence.begin({ after: Concert.Repeating.Loop(4) }); };
+	document.getElementById("GoButton").onclick = function () { mainSequence.begin({ after: Concert.Repeating.Loop(animationLoopCount) }); };
 
 	return mainSequence;
 })();
