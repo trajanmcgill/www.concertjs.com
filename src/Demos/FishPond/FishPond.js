56112cbce3117e894cd22f8c0917285b900f806d
Trajan McGill <code@trajanmcgill.com>
2020-02-28 11:14:49 -0600

    Much of the behavior of the interactive demo now works.
    Issue: #68, #6

--- src/Demos/FishPond/FishPond.js
+++ src/Demos/FishPond/FishPond.js
@@ -4,20 +4,23 @@
 
 	const FishElement = document.getElementById("Fish"),
 		SwimRate = .2, SwimSegmentEasing = Concert.EasingFunctions.QuadInOut,
+		RandomSwimInterval_Min = 5000, RandomSwimInterval_Max = 10000,
 		Background = document.getElementById("Everything");
+
+	let FishBoundingRect = FishElement.getBoundingClientRect();
 
 	const Fish = (function(fishElement, swimRate)
 	{
 		const PathType =
 			{
 				AppendPath: 0,
-				ChangeCourse_BrakeAndChange: 1,
-				ChangeCourse_Gradual: 2,
-				ChangeCourse_Sudden: 3
+				ChangeCourse: 1
 			};
 		
-		let currentStartingPoint = null, currentDestination = null,
-			futureDestinations = [], movementSequence = null;
+		
+		let currentDestination = null, futureDestinations = [],
+			movementSequence = null, currentRandomTimer = null;
+
 
 		function getCurrentPosition()
 		{
@@ -25,39 +28,64 @@
 			return { x: boundingRect.left, y: boundingRect.top };
 		} // end getCurrentPosition()
 
+
 		function calculateDistance(startPosition, endPosition)
 		{
 			return Math.sqrt(Math.pow(endPosition.x - startPosition.x, 2) + Math.pow(endPosition.y - startPosition.y, 2));
 		} // end calculateDistance()
 
-		function calculatePositionAlongLine(lineStart, linePoint2, distance)
-		{
 
-		} // end calculatePositionAlongLine()
-
-		function calculateCurrentRate_QuadInOut(currentTime, startTime, endTime, startPosition, endPosition)
+		function doRandomSwim()
 		{
-			let halfwayTime = (endTime - startTime) / 2,
-				distance = calculateDistance(startPosition, endPosition),
-				rate = distance * (1 / (endTime - halfwayTime) + halfwayTime / Math.pow(endTime - halfwayTime, 2)) - currentTime * distance / Math.pow(endTime - halfwayTime, 2);
-			return rate;
-		} // end calculateCurrentRate_QuadInOut()
+			let backgroundRect = Background.getBoundingClientRect(),
+				randomX = Math.random() * backgroundRect.width,
+				randomY = Math.random() * backgroundRect.height;
+			swimTo({ x: randomX, y: randomY }, PathType.AppendPath, null);
+		} // end doRandomSwim()
+
+
+		function generateRandomSwim()
+		{
+			let randomTimerDuration = RandomSwimInterval_Min + Math.floor((1 + RandomSwimInterval_Max - RandomSwimInterval_Min) * Math.random());
+			currentRandomTimer = window.setTimeout(doRandomSwim, randomTimerDuration);
+		} // end generateRandomSwim()
+
+
+		function begin()
+		{
+			currentRandomTimer = null;
+			generateRandomSwim();
+		} // end begin()
+
+
+		function eat(food)
+		{
+			Fish.swimTo(
+				food.position,
+				Fish.PathType.AppendPath,
+				function() { food.element.parentNode.removeChild(food.element); });
+		} // end eat()
+
 
 		function swimNextSegment()
 		{
 			if(movementSequence instanceof Concert.Sequence)
+			{
 				movementSequence.stop();
+				movementSequence = null;
+			}
 			
 			if(futureDestinations.length < 1)
 			{
-				movementSequence = null;
+				currentDestination = null;
+				generateRandomSwim();
 				return;
 			}
 			
-			currentDestination = futureDestinations.pop();
-			currentStartingPoint = getCurrentPosition();
+			currentDestination = futureDestinations.shift();
 
-			let pathDistance = calculateDistance(currentStartingPoint, currentDestination),
+			let startingPoint = getCurrentPosition(),
+				pathDistance = calculateDistance(startingPoint, currentDestination.position),
 				swimTime = pathDistance / swimRate;
 			
 			movementSequence = new Concert.Sequence();
@@ -72,54 +100,89 @@
 						[{
 							t0: 0,
 							t1: swimTime,
-							v0: [currentStartingPoint.x, currentStartingPoint.y],
-							v1: [currentDestination.x, currentDestination.y]
+							v0: [startingPoint.x, startingPoint.y],
+							v1: [currentDestination.position.x, currentDestination.position.y]
 						}]
 				});
-			movementSequence.begin({ onAutoStop: swimNextSegment });
+			
+			movementSequence.begin(
+				{
+					onAutoStop:
+						function()
+						{
+							if(currentDestination.afterAction)
+								currentDestination.afterAction();
+							swimNextSegment();
+						}
+				});
 		} // end swimNextSegment()
 
-		function swimTo(endPosition, path)
+
+		function swimTo(endPosition, path, afterAction)
 		{
-			if(path === PathType.AppendPath)
-				futureDestinations.push(endPosition);
-			else if(path === PathType.ChangeCourse_BrakeAndChange)
+			if(currentRandomTimer !== null)
 			{
-				if(movementSequence instanceof Concert.Sequence)
-				{
-					let movementRate =
-						calculateCurrentRate_QuadInOut(
-							movementSequence.getCurrentTime(),
-							movementSequence.getStartTime(), movementSequence.getEndTime(),
-							currentStartingPoint, currentDestination);
-					// ADD CODE HERE
-				}
-				else
-					swimTo(endPosition, PathType.ChangeCourse_Sudden);
+				window.clearTimeout(currentRandomTimer);
+				currentRandomTimer = null;
 			}
-			else if(path === PathType.ChangeCourse_Gradual)
+
+			let offsetPosition =
+				{
+					x: endPosition.x - FishBoundingRect.width / 2,
+					y: endPosition.y - FishBoundingRect.height / 2
+				};
+			let newDestination =
+				{
+					position: offsetPosition,
+					afterAction: afterAction
+				};
+
+			if(path === PathType.AppendPath)
 			{
-				// ADD CODE HERE
+				futureDestinations.push(newDestination);
+				if(currentDestination === null)
+					swimNextSegment();
 			}
 			else
-				futureDestinations = [endPosition];
-			
-			if(futureDestinations.length === 1)
+			{
+				futureDestinations = [newDestination];
 				swimNextSegment();
+			}
 		} // end swimTo()
+
 
 		const publicInterface =
 			{
 				PathType: PathType,
+
+				begin: begin,
+				eat: eat,
 				swimTo: swimTo
 			};
 
 		return publicInterface;
-	})(FishElement, SwimRate);
+	})(FishElement, SwimRate); // end Fish singleton
+
+
+	function FishFood(position, backgroundElement)
+	{
+		let element = document.createElement("div");
+		element.className = "FishFood";
+		backgroundElement.appendChild(element);
+		let elementRect = element.getBoundingClientRect();
+		element.style.left = (position.x - elementRect.width / 2) + "px";
+		element.style.top = (position.y - elementRect.height / 2) + "px";
+
+		this.position = position;
+		this.element = element;
+	} // end FishFood constructor
+
 
 	Background.onclick =
 		function(eventObject)
 		{
-			Fish.swimTo({ x: eventObject.clientX, y: eventObject.clientY }, Fish.PathType.ChangeCourse_Sudden);
+			let clickLocation = { x: eventObject.clientX, y: eventObject.clientY };
+			Fish.eat(new FishFood(clickLocation, Background), Fish.PathType.AppendPath);
 		}
+	Fish.begin();
 })();
